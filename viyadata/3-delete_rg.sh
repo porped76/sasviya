15bdea7b879384dd20283865904b3da2557498bd
Pedro Dias <62136185+porped76@users.noreply.github.com>
2021-03-22 20:22:55 +0000

    Update 3-delete_rg.sh

--- viyadata/3-delete_rg.sh
+++ viyadata/3-delete_rg.sh
@@ -1,2 +1,4 @@
+# Define your SAS userid. To avoid any conflict with the Azure created objects, the usage of your $SASUID is advised
+SASUID=$(az ad signed-in-user show --query mailNickname | sed -e 's/^"//' -e 's/"$//')
 # Remove the parent resource group and the entire children objects
 az group delete --resource-group ${SASUID}-rg
